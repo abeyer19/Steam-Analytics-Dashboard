c0f5c53ec19b4cab7c5bb6ebba35e468c037839c
Andrew <abeyer22@outlook.com>
2026-03-31 21:18:17 -0500

    Refactor regression model and save coefficients
    Updated the file path for the master table and modified the model to exclude additional variables. Added code to save model coefficients to a CSV file.

--- Algorithms/units_regression.R
+++ Algorithms/units_regression.R
@@ -1,18 +1,19 @@
-master_data <- read.csv('Transformers/tables/master_table.csv')
+master_data <- read.csv("DVA-Project/Transformers/tables/master_table.csv")
 #colnames(master_data)
 # remove 'type' column for lm function to work
 master_data <- subset(master_data, select = -type)
 
 # use lm function for modeling the dependent variables = units_sold_lowerbound, units_sold, and units_sold_upperbound
 # remove steam_appid, name, and total_reviews from the independent variables list
-model_log <- lm(log(total_reviews + 1) ~. -steam_appid -name -units_sold_lowerbound -units_sold -units_sold_upperbound, data=master_data)
+# also need to remove controller_support_full and review_score_9.0 as those are already in the intercept
+model_log <- lm(log(total_reviews + 1) ~. -steam_appid -name -units_sold_lowerbound -units_sold -units_sold_upperbound -controller_support_full - review_score_9.0, data=master_data)
 summary(model_log)
 
 # when performing predictions, we need to turn the predictions back into real numbers - not log form
 # then multiply this by the thresholds from the python script; 30->base +/- 15
-#log_preds <- predict.lm()
-#real_preds <- exp(log_preds + (summary(model_log)$sigma^2 / 2))
 
-#estimated_units_sold <- real_preds * 30
-#estimated_units_sold_lowerbound  <- real_preds * 15
-#estimated_units_sold_upperbound  <- real_preds * 45
+# Get the coefficients, intercept, and slopes
+coefs <- stack(coef(model_log))
+coefs <- coefs[, c("ind", "values")]
+coefs
+write.csv(coefs, "DVA-Project/Algorithms/model_coefficients.csv", row.names = FALSE)
